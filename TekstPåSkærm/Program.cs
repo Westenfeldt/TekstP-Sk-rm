1c50fc554fbba0249209f2ad05fb3602f2b57524
Stefan Vestenfeldt <stefanwestenfeldt@gmail.com>
2019-08-13 12:21:53 +0200

    Add project files.

---
+++ TekstPåSkærm/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TekstPåSkærm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Beep(); 
+           
+
+            Console.Write("test");
+        }
+    }
+}
